04dbc95c00b963d09fdef9d9be4d445b661c3537
Seol_JY <wlsdud5654@gmail.com>
2024-04-21 00:06:49 +0900

    refactor: auth 도메인과 user 도메인에 대한 구분

--- src/main/java/one/colla/common/security/authentication/UserDetailServiceImpl.java
+++ src/main/java/one/colla/common/security/authentication/UserDetailServiceImpl.java
@@ -6,17 +6,17 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import one.colla.user.application.UserService;
+import one.colla.auth.application.AuthService;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailServiceImpl implements UserDetailsService {
 	private static final String NOT_FOUND_MESSAGE = "사용자를 찾을 수 없습니다.";
-	private final UserService userService;
+	private final AuthService authService;
 
 	@Override
 	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-		return userService.getUserById(Long.parseLong(userId))
+		return authService.getUserById(Long.parseLong(userId))
 			.map(CustomUserDetails::from)
 			.orElseThrow(() -> new UsernameNotFoundException(NOT_FOUND_MESSAGE));
 	}
