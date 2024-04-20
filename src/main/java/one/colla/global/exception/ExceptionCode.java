e813905b455ef28d5edd76d03ab9b95450867631
Seol_JY <wlsdud5654@gmail.com>
2024-04-21 05:35:54 +0900

    feat: 예외코드 추가

--- src/main/java/one/colla/global/exception/ExceptionCode.java
+++ src/main/java/one/colla/global/exception/ExceptionCode.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ExceptionCode {
 	/* 401_UNAUTHORIZED */
+	INVALID_USERNAME_OR_PASSWORD(HttpStatus.UNAUTHORIZED, 40102, "이메일 또는 비밀번호가 일치하지 않습니다."),
 	EMPTY_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, 40182, "토큰이 포함되어 있지 않습니다."),
 	EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, 40183, "사용기간이 만료된 토큰입니다."),
 	MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED, 40184, "비정상적인 토큰입니다."),
@@ -17,6 +18,7 @@ public enum ExceptionCode {
 
 	/* 403_FORBIDDEN */
 	FORBIDDEN_ACCESS_TOKEN(HttpStatus.FORBIDDEN, 40181, "토큰에 접근 권한이 없습니다."),
+	TAKEN_AWAY_TOKEN(HttpStatus.UNAUTHORIZED, 40187, "인증 불가, 관리자에게 문의하세요."),
 
 	/* 500_INTERNAL_SERVER_ERROR */
 	UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 50001, "서버 에러 입니다.");
