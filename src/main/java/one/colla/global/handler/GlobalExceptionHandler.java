24bc0d1e9ec10d16b9738b6bf6c213eab4025b75
Seol_JY <wlsdud5654@gmail.com>
2024-04-28 02:44:29 +0900

    feat: 전역 예외 처리를 ResponseEntityExceptionHandler를 상속받는 형태로 변경

--- src/main/java/one/colla/global/handler/GlobalExceptionHandler.java
+++ src/main/java/one/colla/global/handler/GlobalExceptionHandler.java
@@ -3,11 +3,19 @@ package one.colla.global.handler;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import io.jsonwebtoken.ExpiredJwtException;
@@ -15,15 +23,16 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import one.colla.common.presentation.ApiResponse;
 import one.colla.global.exception.CommonException;
 import one.colla.global.exception.ExceptionCode;
 import one.colla.global.exception.VoException;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	private static final Map<Class<? extends JwtException>, ExceptionCode> JWT_EXCEPTION_CODE_MAP = Map.of(
 		ExpiredJwtException.class, ExceptionCode.EXPIRED_TOKEN,
 		MalformedJwtException.class, ExceptionCode.MALFORMED_TOKEN,
@@ -36,9 +45,30 @@ public class GlobalExceptionHandler {
 		return ApiResponse.createErrorResponseEntity(ex);
 	}
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationExceptions(
-		MethodArgumentNotValidException ex) {
+	@ExceptionHandler(JwtException.class)
+	public ResponseEntity<Object> handleJwtException(JwtException ex) {
+		Class<? extends Exception> exceptionClass = ex.getClass();
+		ExceptionCode exceptionCode = JWT_EXCEPTION_CODE_MAP.get(exceptionClass);
+		return ApiResponse.createErrorResponseEntity(exceptionCode);
+	}
+
+	@ExceptionHandler(VoException.class)
+	public ResponseEntity<ApiResponse<String>> handleVoErrorExceptions(VoException ex) {
+		return ApiResponse.createErrorResponseEntity(ex);
+	}
+
+	@Override
+	@Nullable
+	protected ResponseEntity<Object> handleNoResourceFoundException(
+		NoResourceFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		return ApiResponse.createErrorResponseEntity(ExceptionCode.NOT_FOUND_RESOURCE);
+	}
+
+	@Override
+	@Nullable
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 		Map<String, String> errors = new HashMap<>();
 		ex.getBindingResult().getAllErrors().forEach(error -> {
 			String fieldName = ((FieldError)error).getField();
@@ -49,25 +79,38 @@ public class GlobalExceptionHandler {
 		return ApiResponse.createValidationResponseEntity(errors);
 	}
 
-	@ExceptionHandler(JwtException.class)
-	public ResponseEntity<ApiResponse<CommonException>> handleJwtException(JwtException ex) {
-		Class<? extends Exception> exceptionClass = ex.getClass();
-		ExceptionCode exceptionCode = JWT_EXCEPTION_CODE_MAP.get(exceptionClass);
-		return ApiResponse.createErrorResponseEntity(exceptionCode);
+	@Override
+	@Nullable
+	protected ResponseEntity<Object> handleMissingServletRequestParameter(
+		MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		String missingParam = ex.getParameterName();
+		Map<String, String> errors = new HashMap<>();
+		errors.put(missingParam, String.format("필수 쿼리 파라미터 '%s'가 누락되었습니다.", missingParam));
+		return ApiResponse.createValidationResponseEntity(errors);
 	}
 
-	@ExceptionHandler(NoResourceFoundException.class)
-	public ResponseEntity<ApiResponse<Map<String, String>>> handleNoResourceExceptions(Exception ex) {
-		return ApiResponse.createErrorResponseEntity(ExceptionCode.NOT_FOUND_RESOURCE);
+	@Override
+	@Nullable
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+		HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		return ApiResponse.createErrorResponseEntity(ExceptionCode.METHOD_FORBIDDEN);
 	}
 
-	@ExceptionHandler(VoException.class)
-	public ResponseEntity<ApiResponse<String>> handleVoErrorExceptions(VoException ex) {
-		return ApiResponse.createErrorResponseEntity(ex);
-	}
+	@Override
+	@Nullable
+	protected ResponseEntity<Object> handleExceptionInternal(
+		Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ApiResponse<Map<String, String>>> handleUnexpectedExceptions(Exception ex) {
+		if (request instanceof ServletWebRequest servletWebRequest) {
+			HttpServletResponse response = servletWebRequest.getResponse();
+			if (response != null && response.isCommitted()) {
+				if (logger.isWarnEnabled()) {
+					logger.warn("Response already committed. Ignoring: " + ex);
+				}
+				return null;
+			}
+		}
+		logger.error("Unexpected error 발생: " + ex.getMessage(), ex);
 		return ApiResponse.createServerErrorResponseEntity();
 	}
 }
