45fcf633975e5622656af2c1295378a2adbda3dd
Junad-Park <qkrwnsgk1624@icloud.com>
2024-05-06 05:20:14 +0900

    feat: provider에 해당하는 oauth 로그인 uri 생성 api 개발

--- src/main/java/one/colla/auth/presentation/AuthController.java
+++ src/main/java/one/colla/auth/presentation/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,22 +21,49 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import one.colla.auth.application.AuthService;
+import one.colla.auth.application.OAuthService;
 import one.colla.auth.application.dto.JwtPair;
 import one.colla.auth.application.dto.request.LoginRequest;
+import one.colla.auth.application.dto.request.OAuthLoginRequest;
 import one.colla.auth.application.dto.request.RegisterRequest;
 import one.colla.auth.application.dto.request.VerificationCheckRequest;
 import one.colla.auth.application.dto.request.VerifyMailSendRequest;
 import one.colla.auth.application.dto.response.LoginResponse;
+import one.colla.auth.application.dto.response.OauthLoginUrlResponse;
+import one.colla.auth.config.OAuthProperties;
+import one.colla.auth.config.OAuthPropertyFactory;
 import one.colla.common.presentation.ApiResponse;
 import one.colla.common.util.CookieUtil;
+import one.colla.common.util.oauth.OAuthUriGenerator;
+import one.colla.user.domain.Provider;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+
 	private static final int REFRESH_TOKEN_EXPIRES_IN_DAYS = 7;
+
 	private final AuthService authService;
 	private final CookieUtil cookieUtil;
+	private final OAuthPropertyFactory oAuthPropertyFactory;
+	private final OAuthUriGenerator oAuthUriGenerator;
+	private final OAuthService oAuthService;
+
+	@GetMapping("/oauth/{provider}/login")
+	public ResponseEntity<ApiResponse<OauthLoginUrlResponse>> getOAuthUrl(
+		@PathVariable(value = "provider") final Provider provider) {
+		OAuthProperties oAuthProperties = oAuthPropertyFactory.createOAuthProperty(provider);
+		OauthLoginUrlResponse oauthUrl = oAuthUriGenerator.generate(oAuthProperties);
+		return ResponseEntity.ok()
+			.body(ApiResponse.createSuccessResponse(oauthUrl));
+	}
+
+	@PostMapping("/oauth/{provider}/code")
+	public ResponseEntity<ApiResponse<LoginResponse>> oauthRegisterOrLogin(
+		@PathVariable("provider") final Provider provider, @RequestBody @Valid OAuthLoginRequest request) {
+		return createAuthResponse(oAuthService.createToken(request, provider));
+	}
 
 	@PostMapping("/login")
 	@PreAuthorize("isAnonymous()")
