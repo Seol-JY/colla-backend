8026b5c60825756c02b767a30e6f0f3cecbd1491
Seol_JY <wlsdud5654@gmail.com>
2024-05-07 19:34:45 +0900

    feat: cors 허용 설정

--- src/main/java/one/colla/global/config/security/CorsConfig.java
+++ src/main/java/one/colla/global/config/security/CorsConfig.java
@@ -18,8 +18,11 @@ public class CorsConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		// TODO: 추후 AllowOrigins 설정 필요
-		// configuration.setAllowedOrigins(List.of());
+		configuration.setAllowedOrigins(List.of(
+			"https://api.colla.so",
+			"https://colla.so",
+			"http://localhost:3000"
+		));
 		configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.SET_COOKIE));
