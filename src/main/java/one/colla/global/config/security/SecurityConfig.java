8b9ab4447efc5fb3c8035d781a0eb2ad270c4599
Junad-Park <qkrwnsgk1624@icloud.com>
2024-06-03 22:52:32 +0900

    fix: cors 정책 변경

--- src/main/java/one/colla/global/config/security/SecurityConfig.java
+++ src/main/java/one/colla/global/config/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -52,6 +53,8 @@ public class SecurityConfig {
 
 	private HttpSecurity defaultSecurity(HttpSecurity http) throws Exception {
 		return http.httpBasic(AbstractHttpConfigurer::disable)
+			.headers(headers -> headers
+				.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
 			.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(sessionManagement ->
 				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
