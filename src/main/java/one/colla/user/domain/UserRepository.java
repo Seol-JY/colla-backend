420cf3acc0f932e0b78c9bb8ebe9da89f53f8d65
Seol_JY <wlsdud5654@gmail.com>
2025-03-04 20:36:41 +0900

    feat: status Api 내부 쿼리 성능 개선

--- src/main/java/one/colla/user/domain/UserRepository.java
+++ src/main/java/one/colla/user/domain/UserRepository.java
@@ -3,9 +3,17 @@ package one.colla.user.domain;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import one.colla.user.domain.vo.Email;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(Email email);
+
+	@Query("SELECT DISTINCT u FROM User u "
+		+ "LEFT JOIN FETCH u.userTeamspaces ut "
+		+ "LEFT JOIN FETCH ut.teamspace t "
+		+ "WHERE u.id = :userId")
+	Optional<User> findByIdWithTeamspaces(@Param("userId") Long userId);
 }
